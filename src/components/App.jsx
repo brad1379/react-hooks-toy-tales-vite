f30bd33e7ba65713e80b2fec1bd98dd3ffe0ecc5
Brad Arnold <brad1379@gmail.com>
2026-07-12 14:21:11 -0500

    added the toys to the page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
@@ -6,6 +6,17 @@ import ToyContainer from "./ToyContainer";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [toys, setToys] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/toys")
+    .then(response => {
+      if (!response.ok) {throw new Error("")}
+      return response.json()
+    })
+    .then(data => setToys(data))
+    .catch(error => console.log(error.message))
+  })
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
@@ -18,7 +29,7 @@ function App() {
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer toys={toys}/>
     </>
   );
 }
